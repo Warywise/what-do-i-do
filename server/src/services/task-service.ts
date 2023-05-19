e596e89093eddabc0b3d791bd6239bb756b4bd4e
Gustavo Sant'Anna <g_santanna@outlook.com>
2023-05-19 18:50:45 -0300

    feat(server): adds createTask method on tasks service

--- server/src/services/task-service.ts
+++ server/src/services/task-service.ts
@@ -11,9 +11,28 @@ class TaskService {
   }
 
   getTasks() {
-      const tasks = this.Storage.READ_FILE();
+    const tasks = this.Storage.READ_FILE();
 
-      return tasks;
+    return tasks;
+  }
+
+  createTask(req: Request) {
+    const { title, description, category } = req.body as { [key: string]: string };
+    const tasks = this.Storage.READ_FILE();
+
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      createdAt: new Date(),
+      concludedAt: null,
+    };
+
+    tasks[category].push(newTask);
+
+    this.Storage.WRITE_FILE(tasks);
+
+    return newTask;
   }
 };
 
