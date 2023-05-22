bc5f969624f0f192e2c8395600b0512154e0edee
Gustavo Sant'Anna <g_santanna@outlook.com>
2023-05-22 13:23:44 -0300

    test(server): implements integration tests   - tests server basic features   - tests tasks CRUD   - tests categories CRUD

---
+++ server/src/tests/index.test.ts
@@ -0,0 +1,7 @@
+import { serverTests } from './server.test';
+import { tasksTests } from './tasks.test';
+import { categoriesTests } from './categories.test';
+
+serverTests;
+tasksTests;
+categoriesTests;
--- server/src/tests/server.test.ts
+++ server/src/tests/server.test.ts
@@ -1,9 +1,12 @@
 import request from 'supertest';
+import { resolve as resolvePath } from 'path';
+
 import server from '../server';
 import HTTP_CODES from '../lib/httpCodes';
+import * as Storage from '../lib/handleFile';
 
-describe('Test the root path', () => {
-  test('It should response the GET method', async () => {
+export const serverTests = describe('Test the essential server features.', () => {
+  test('It should response the GET method at root path', async () => {
     const response = await request(server).get('/');
 
     expect(response.statusCode).toEqual(HTTP_CODES.OK);
@@ -13,7 +16,24 @@ describe('Test the root path', () => {
   test('It should throw an Error', async () => {
     const response = await request(server).get('/').send({ error: true });
 
-    expect(response.statusCode).toEqual(HTTP_CODES.INTERNAL_SERVER_ERROR);
     expect(response.body).toEqual({ error: 'Erro de teste' });
+  });
+
+  test('It should handle the file correctly', () => {
+    const path = resolvePath(__dirname, 'tasks_test.json');
+    const testFile1 = Storage.READ_FILE(path);
+
+    expect(testFile1).toHaveProperty('general');
+    expect(Array.isArray(testFile1.general)).toBeTruthy();
+    expect(testFile1.general.length).toBeGreaterThan(0);
+
+    Storage.WRITE_FILE({ test: [] }, path);
+    const testFile2 = Storage.READ_FILE(path);
+
+    expect(testFile2).toHaveProperty('test');
+    expect(Array.isArray(testFile2.test)).toBeTruthy();
+    expect(testFile2.test.length).toEqual(0);
+
+    Storage.WRITE_FILE(testFile1, path);
   });
 });
