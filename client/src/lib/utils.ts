42acd6a962c79bc162e86f4d0d1883b72440027f
Gustavo Sant'Anna <g_santanna@outlook.com>
2023-05-25 20:41:56 -0300

    feat(client): functions to handle boards color

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -30,10 +30,43 @@ export const deleteTask = async (task: DeleteTaskPayload): Promise<TasksData> =>
 
 export const CATEGORY_URL = 'http://localhost:5000/category';
 
-export const createBoard = async (category: string): Promise<TasksData> => {
-  return await fetcher(CATEGORY_URL, 'POST', { category });
+export const createBoard = async (name: string): Promise<TasksData> => {
+  return await fetcher(CATEGORY_URL, 'POST', { name });
 }
 
 export const deleteBoard = async (category: string): Promise<TasksData> => {
   return await fetcher(CATEGORY_URL, 'DELETE', { category });
+}
+
+const colors = [
+  '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+  '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+  '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
+  '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+  '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
+  '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+  '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
+  '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+  '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
+  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'
+];
+
+export const getRandomColor = (): string => {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
+
+export const getBoardsColor = (category?: string) => {
+  const boardsColor = JSON.parse(localStorage.getItem('boardsColor') || '{}') as { [k:string]: string };
+  if (category) {
+    const color = boardsColor[category];
+    return color ?? getRandomColor();
+  }
+
+  return boardsColor;
+}
+
+export const setBoardsColor = (category: string, color: string): void => {
+  const boardsColor = getBoardsColor() as { [k:string]: string };
+  boardsColor[category] = color;
+  localStorage.setItem('boardsColor', JSON.stringify(boardsColor));
 }
