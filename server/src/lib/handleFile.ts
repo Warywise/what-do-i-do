96b632b247ef2638600f0145b493d0082f8e4d2f
Gustavo Sant'Anna <g_santanna@outlook.com>
2023-05-27 23:41:57 -0300

    feat(server/client): taking measures to ensure tasks file

--- server/src/lib/handleFile.ts
+++ server/src/lib/handleFile.ts
@@ -4,7 +4,16 @@ import { TaskFile } from './taskTypes';
 
 const filePath = path.join(__dirname, '..', 'tasks_storage.json');
 
-export const READ_FILE = (customPath ?: string): TaskFile => {
+export const EXISTS_FILE = (customPath?: string): boolean => {
+  try {
+    return fs.existsSync(customPath || filePath);
+  } catch (error) {
+    if (error instanceof Error) console.log('File Error: ', error?.message);
+    return false;
+  }
+};
+
+export const READ_FILE = (customPath?: string): TaskFile => {
   const data = fs.readFileSync(customPath || filePath);
   const file = JSON.parse(data.toString());
 
